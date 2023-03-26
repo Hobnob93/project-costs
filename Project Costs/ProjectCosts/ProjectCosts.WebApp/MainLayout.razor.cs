f4144ca3ac8b28269bf04f53123d3c3bba4897dd
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2023-03-26 16:19:29 +0100

    Set up mudblazor basics

---
+++ Project Costs/ProjectCosts/ProjectCosts.WebApp/MainLayout.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectCosts.Web;
+
+public partial class MainLayout
+{
+    private bool _drawerOpen = false;
+
+    private void ToggleDrawer()
+    {
+        _drawerOpen = !_drawerOpen;
+    }
+}
--- Project Costs/ProjectCosts/ProjectCosts.WebApp/Program.cs
+++ Project Costs/ProjectCosts/ProjectCosts.WebApp/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
+using MudBlazor.Services;
 using ProjectCosts.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+});
 
 await builder.Build().RunAsync();
