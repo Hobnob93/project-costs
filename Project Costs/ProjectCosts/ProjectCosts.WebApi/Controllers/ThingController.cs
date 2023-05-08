49fb25ca57021d4d9a5ad0367ff338087b945835
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2023-05-08 19:59:25 +0100

    Gave test data fixed IDs

--- Project Costs/ProjectCosts/ProjectCosts.WebApi/Controllers/ThingController.cs
+++ Project Costs/ProjectCosts/ProjectCosts.WebApi/Controllers/ThingController.cs
@@ -89,7 +89,7 @@ public class ThingController : ControllerBase
     {
         new ThingOverview
         {
-            Id = Guid.NewGuid().ToString(),
+            Id = "03B39058-07DE-4D3F-8C2C-A8EBD5FF9D2A",
             Name = "New Patio",
             Cost = new Cost
             {
@@ -101,7 +101,7 @@ public class ThingController : ControllerBase
         },
         new ThingOverview
         {
-            Id = Guid.NewGuid().ToString(),
+            Id = "C979EA94-8576-450A-B40A-8A4AC525F8DF",
             Name = "New Zealand Holiday",
             Cost = new Cost
             {
@@ -117,7 +117,7 @@ public class ThingController : ControllerBase
         },
         new ThingOverview
         {
-            Id = Guid.NewGuid().ToString(),
+            Id = "6EDDAD2C-BC13-4F11-A040-691FD0F84326",
             Name = "LAN i70",
             Cost = new Cost
             {
@@ -129,7 +129,7 @@ public class ThingController : ControllerBase
         },
         new ThingOverview
         {
-            Id = Guid.NewGuid().ToString(),
+            Id = "0321535A-7025-4F1B-91D7-95CBBF23793D",
             Name = "Amber's Birthday",
             Cost = new Cost
             {
@@ -141,7 +141,7 @@ public class ThingController : ControllerBase
         },
         new ThingOverview
         {
-            Id = Guid.NewGuid().ToString(),
+            Id = "75825BC1-234C-410D-A2E5-CB6AF5FE2863",
             Name = "Christmas 2023",
             Cost = new Cost
             {
@@ -153,7 +153,7 @@ public class ThingController : ControllerBase
         },
         new ThingOverview
         {
-            Id = Guid.NewGuid().ToString(),
+            Id = "820AA71E-02F7-4F0C-8A5E-67E838B88979",
             Name = "Some Random Thing",
             Cost = new Cost
             {
@@ -164,7 +164,7 @@ public class ThingController : ControllerBase
         },
         new ThingOverview
         {
-            Id = Guid.NewGuid().ToString(),
+            Id = "696603C4-5C84-4FFD-9B82-76628D546414",
             Name = "Some Other Random Thing",
             Cost = new Cost
             {
